8b44093b14009222bb0e608a95c096299ad8970f
zhenqiang <qiang.zhen@dnurse.cn>
2016-12-09 22:51:09 +0800

    android自带类实现语音播放（英文）

---
+++ SpeechUtils.java
@@ -0,0 +1,56 @@
+package com.dnurse.common.utils;
+
+import android.content.Context;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+/**
+ * Created by zhenqiang on 2016/12/9.
+ */
+
+public class SpeechUtils {
+    private Context context;
+
+
+    private static final String TAG = "SpeechUtils";
+    private static SpeechUtils singleton;
+
+    private TextToSpeech textToSpeech; // TTS对象
+
+    public static SpeechUtils getInstance(Context context) {
+        if (singleton == null) {
+            synchronized (SpeechUtils.class) {
+                if (singleton == null) {
+                    singleton = new SpeechUtils(context);
+                }
+            }
+        }
+        return singleton;
+    }
+
+    public SpeechUtils(Context context) {
+        this.context = context;
+        textToSpeech = new TextToSpeech(context, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if (i == TextToSpeech.SUCCESS) {
+                    textToSpeech.setLanguage(Locale.US);
+                    textToSpeech.setPitch(1.0f);// 设置音调，值越大声音越尖（女生），值越小则变成男声,1.0是常规
+                    textToSpeech.setSpeechRate(1.0f);
+                }
+            }
+        });
+    }
+
+    public void speakText(String text) {
+        if (textToSpeech != null) {
+            textToSpeech.speak(text,
+                    TextToSpeech.QUEUE_FLUSH, null);
+        }
+
+    }
+
+}
